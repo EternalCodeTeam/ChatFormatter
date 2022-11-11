22dcce836fc722d876ef57576ef0eb713871fc42
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2022-11-11 23:15:37 +0100

    Delete unused repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,6 @@ subprojects {
         maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
         maven { url = uri("https://repo.panda-lang.org/releases") }
         maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
-        maven { url = uri("https://repo.eternalcode.pl/releases") }
-        maven { url = uri("https://repository.minecodes.pl/releases") }
         maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
         maven { url = uri("https://jitpack.io") }
     }
