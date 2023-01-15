fd0d3884f28a00c17145e06aba13ff3693558fde
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-15 15:20:31 +0100

    Update UpdaterService.java

--- chat-formatter/src/com/eternalcode/formatter/updater/UpdaterService.java
+++ chat-formatter/src/com/eternalcode/formatter/updater/UpdaterService.java
@@ -6,12 +6,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class UpdaterService {
 
     private static final String UPDATE_URL = "https://raw.githubusercontent.com/EternalCodeTeam/ChatFormatter/create-updater/version.txt";
+
+    private static final String NEW_VERSION_AVAILABLE = "<b><gradient:#29fbff:#38b3ff>ChatFormatter:</gradient></b> <green>New version of ChatFormatter is available, please update!";
+    private static final String NO_NEW_VERSION_AVAILABLE = "<b><gradient:#29fbff:#38b3ff>ChatFormatter:</gradient></b> <green>ChatFormatter is up to date!";
 
     private final MiniMessage miniMessage;
 
@@ -27,11 +31,11 @@ public class UpdaterService {
             String versionFromFile = in.readLine();
 
             if (versionFromFile.equals(plugin.getDescription().getVersion())) {
-                player.sendMessage(Legacy.LEGACY_AMPERSAND_SERIALIZER.serialize(this.miniMessage.deserialize("<b><gradient:#29fbff:#38b3ff>ChatFormatter:</gradient></b> <green>ChatFormatter is up to date!")));
+                player.sendMessage(Legacy.LEGACY_AMPERSAND_SERIALIZER.serialize(this.miniMessage.deserialize(NO_NEW_VERSION_AVAILABLE)));
             } else {
-                player.sendMessage(Legacy.LEGACY_AMPERSAND_SERIALIZER.serialize(this.miniMessage.deserialize("<b><gradient:#29fbff:#38b3ff>ChatFormatter:</gradient></b> <green>New version of ChatFormatter is available, please update!")));
+                player.sendMessage(Legacy.LEGACY_AMPERSAND_SERIALIZER.serialize(this.miniMessage.deserialize(NEW_VERSION_AVAILABLE)));
             }
         }
-        catch (Exception ignored) {}
+        catch (IOException ignored) {}
     }
 }
