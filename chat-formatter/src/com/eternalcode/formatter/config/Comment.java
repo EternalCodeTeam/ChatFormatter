7b14ab45cc57d32f10092755af7763a48690ff02
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-18 21:09:51 +0100

    Create simple annotations for temponary resolve cdn description duplication bug

---
+++ chat-formatter/src/com/eternalcode/formatter/config/Comment.java
@@ -0,0 +1,14 @@
+package com.eternalcode.formatter.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Comments.class)
+public @interface Comment {
+    String[] value();
+}
