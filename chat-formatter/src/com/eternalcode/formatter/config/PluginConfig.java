e12250f65ef06aee6c5d02ebbe808b73c053e17b
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2023-01-19 23:06:06 +0100

    Bump CDN configs to 1.14.3 and remove annotations for temponary fix bug from 1.14.2

--- chat-formatter/src/com/eternalcode/formatter/config/PluginConfig.java
+++ chat-formatter/src/com/eternalcode/formatter/config/PluginConfig.java
@@ -6,35 +6,36 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.eternalcode.formatter.ChatSettings;
 import com.eternalcode.formatter.placeholder.PlaceholderStack;
+import net.dzikoysk.cdn.entity.Description;
 
 import java.util.List;
 import java.util.Map;
 
 public class PluginConfig implements ChatSettings, PlaceholderStack, TemplateRepository {
 
-    @Comment({ " " })
-    @Comment("#    ____ _           _   _____      ChatFormatter       _   _            ")
-    @Comment("#   / ___| |__   __ _| |_|  ___|__  _ __ _ __ ___   __ _| |_| |_ ___ _ __ ")
-    @Comment("#  | |   | '_ \\ / _` | __| |_ / _ \\| '__| '_ ` _ \\ / _` | __| __/ _ \\ '__|")
-    @Comment("#  | |___| | | | (_| | |_|  _| (_) | |  | | | | | | (_| | |_| ||  __/ |   ")
-    @Comment("#   \\____|_| |_|\\__,_|\\__|_|  \\___/|_|  |_| |_| |_|\\__,_|\\__|\\__\\___|_|   ")
-    @Comment({ " " })
+    @Description(" ")
+    @Description("#    ____ _           _   _____      ChatFormatter       _   _            ")
+    @Description("#   / ___| |__   __ _| |_|  ___|__  _ __ _ __ ___   __ _| |_| |_ ___ _ __ ")
+    @Description("#  | |   | '_ \\ / _` | __| |_ / _ \\| '__| '_ ` _ \\ / _` | __| __/ _ \\ '__|")
+    @Description("#  | |___| | | | (_| | |_|  _| (_) | |  | | | | | | (_| | |_| ||  __/ |   ")
+    @Description("#   \\____|_| |_|\\__,_|\\__|_|  \\___/|_|  |_| |_| |_|\\__,_|\\__|\\__\\___|_|   ")
+    @Description(" ")
 
-    @Comment({ " ", "# Do you want to use pre-chat format? (Other plugins could add their custom prefixes etc.)" })
-    @Comment("# INFO: This option requires to use custom badges like {displayname} and {message} in each message.")
+    @Description({ " ", "# Do you want to use pre-chat format? (Other plugins could add their custom prefixes etc.)" })
+    @Description("# INFO: This option requires to use custom badges like {displayname} and {message} in each message.")
     public boolean preFormatting = false;
 
     public String defaultFormat = "{displayname} {arrow_right} {message}";
 
-    @Comment({ " ", "# Do you want to receive updates about new versions of ChatFormatter?" })
+    @Description({ " ", "# Do you want to receive updates about new versions of ChatFormatter?" })
     public boolean receiveUpdates = true;
 
 
-    @Comment({ " ", "# Chat format for ranks (Vault) Support mini-messages and legacy colors" })
-    @Comment({ " ", "# We're recommending to use webui for mini-messages: https://webui.adventure.kyori.net/" })
-    @Comment({ " ", "# documentation is here: https://docs.adventure.kyori.net/minimessage/format.html" })
+    @Description({ " ", "# Chat format for ranks (Vault) Support mini-messages and legacy colors" })
+    @Description({ " ", "# We're recommending to use webui for mini-messages: https://webui.adventure.kyori.net/" })
+    @Description({ " ", "# documentation is here: https://docs.adventure.kyori.net/minimessage/format.html" })
 
-    @Comment({
+    @Description({
         "# Example usages:",
         "# ",
         "# Hover message:",
@@ -57,12 +58,12 @@ public class PluginConfig implements ChatSettings, PlaceholderStack, TemplateRep
         .build();
 
 
-    @Comment({ " ", "# It is used to shorten the text even more and keep the clean file!" })
+    @Description({ " ", "# It is used to shorten the text even more and keep the clean file!" })
     public List<Template> templates = new ImmutableList.Builder<Template>()
         .add(Template.of("template", List.of("rank", "color"), "$rank $color{displayname} &8{arrow_right} $color{message}"))
         .build();
 
-    @Comment({ " ", "# Placeholders, it allows you to make a shorter text, you can use some prefixes, characters etc. " })
+    @Description({ " ", "# Placeholders, it allows you to make a shorter text, you can use some prefixes, characters etc. " })
     public Map<String, String> placeholders = new ImmutableMap.Builder<String, String>()
         .put("{displayname}", "<displayname>")
         .put("{message}", "<message>")
